7b68b8f38ec100c00128a9e953e688092f776379
Anastasija Tortevska <atortevska@student.ethz.ch>
2026-04-29 18:44:22 +0200

    Add Python wrapper and tests for C++ attention

--- src/ezgatr/nn/functional/__init__.py
+++ src/ezgatr/nn/functional/__init__.py
@@ -1,5 +1,6 @@
 from .activation import scaler_gated_gelu
 from .attention import compute_qk_for_daa, compute_qk_for_ipa, equi_geometric_attention
+from .attention_cpp import equi_geometric_attention_cpp
 from .dual import equi_dual, equi_join
 from .linear import equi_linear, geometric_product, inner_product, outer_product
 from .norm import equi_rms_norm
@@ -9,6 +10,7 @@ __all__ = [
     "compute_qk_for_ipa",
     "equi_dual",
     "equi_geometric_attention",
+    "equi_geometric_attention_cpp",
     "equi_join",
     "equi_linear",
     "equi_rms_norm",
